cb8cf690f88f21f0b15e2ae40143523242fddfad
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-09-11 21:24:19 +0600

    Less Time complexity
    TwoSum solution with better time complexity using dictionary

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums:list[int],target: int)-> list[int]:
+        visited={}
+        for index in range(0, len(nums)):
+            number = nums[index]
+            if target- number in visited:
+                return[visited[target-number],index]
+            visited[number] = index
+sol= Solution()
+print(sol.twoSum([2,7,11,15],9))
+print(sol.twoSum([3,2,4],6))
+print(sol.twoSum([3,3],6))
+    
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(sol.twoSum([2,7,11,15],9))
+print(sol.twoSum([3,2,4],6))
+print(sol.twoSum([3,3],6))
